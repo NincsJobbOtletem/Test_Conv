388309ac2f24561113fc359a2a20ff2ea1856f25
NincsJobbOtletem <patrikvok123312@gmail.com>
2021-12-06 09:48:29 +0100

    Update

--- src/Modell/db_Connect.php
+++ src/Modell/db_Connect.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+//Legyen globál
 class DatabaseIn{ //DatabaseIn tulajdonságai
   protected $dbservname;
   protected $dbUser;
