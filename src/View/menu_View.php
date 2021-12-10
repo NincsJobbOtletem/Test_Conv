4f2f142ac03350f81704b4c703db776313b838a8
NincsJobbOtletem <patrikvok123312@gmail.com>
2021-12-10 15:30:32 +0100

    uppp no controller now :D

--- src/View/menu_View.php
+++ src/View/menu_View.php
@@ -1,30 +1,56 @@
 <?php
-    class View{
-        protected $body;
-        protected $head;
-        protected $footer;
-        
-        function __construct()
-        {
-            $this->body = file_get_contents(__DIR__ .'/../View/body.html');
-            $this->head = file_get_contents(__DIR__ .'/../View/head.html');
-            $this->footer = file_get_contents(__DIR__ .'/../View/footer.html');
-        }
+class View
+{
+    
+    private $data;
 
-        function body(){
-            return $this->body;
-        }
-        function footer(){
-            return $this->footer;
-        }
-        function head(){
-            return $this->head;
-        }
-        public function run(){
-            $body = $this->body($this->body);
-            $head= $this->head($this->head);
-            $footer = $this->footer($this->footer);
-            return $body.''.$footer.''.$head;
+    function __construct($data)
+    {
+        $this->body = file_get_contents(__DIR__ . '/../View/body.html');
+        $this->head = file_get_contents(__DIR__ . '/../View/head.html');
+        $this->footer = file_get_contents(__DIR__ . '/../View/footer.html');
+        $this->data = $data;
     }
 
+    function body()
+    {
+        return $this->body;
+    }
+    function footer()
+    {
+        return $this->footer;
+    }
+    function head()
+    {
+        return $this->head;
+    }
+    function has_children($rows, $id)
+    {
+        foreach ($rows as $row) {
+            if ($row["parent_id"] == $id)
+                return true;
+        }
+        return false;
+    }
+    function BuildMenu($data, $parent = 0)
+    {
+        $menu = "<ul>";
+        foreach ($data as $row) {
+            if ($row["parent_id"] == $parent) {
+                $menu .= "<li>" . $row["name"];
+                if ($this->has_children($data, $row["id"])) {
+                    $menu .= $this->BuildMenu($data, $row["id"]);
+                }
+                $menu .= "</li>";
+            }
+        }
+
+        $menu .= "</ul>";
+        return $menu;
+    }
+    public function show()
+    {
+        return $this->head().''.$this->body().''.$this->BuildMenu($this->data).''.$this->footer();
+        
+    }
 }
--- src/index.php
+++ src/index.php
@@ -7,11 +7,7 @@ include 'Modell/model_Menu.php';
 $testa = new ConfToData();
 $data = $testa->run();
 
+$viewAndBuild = new View($data);
+print $viewAndBuild->show();
 
-$testb = new Builder($data);
-$menu1 = $testb->run();
-
-$testc = new View();
-$menu = $testc->head().''.$testc->body().''.$menu1.''.$testc->footer();
-print $menu;
 
